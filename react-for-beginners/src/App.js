2b95382f9a9cd8ce88eb44ef9894e3dbf4a61346
jyhynn <letsjamm@naver.com>
2023-12-08 17:07:23 +0900

    update

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,27 +1,9 @@
 import { useState, useEffect } from "react";
 
-function Hello(){
-  // const byeFn = ()=> console.log("bye;(");
-  // const hiFn = () => {
-  //   console.log("created:)")
-  //   return byeFn;
-  // };
-  // useEffect(hiFn,[]);
-
-  useEffect(()=>{
-    console.log("hi:)")
-    return () => console.log("bye:(");
-  },[]);
-  return <h1>Hello</h1>
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing(prev => !prev);
   return (
     <div>
-      {showing ? <Hello/> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      hi
     </div>
   );
 }
